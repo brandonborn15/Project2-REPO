a765ea961773f631bfe871b4ad9b1b549712c6ae
brandon <btm1998x@gmail.com>
2018-10-18 19:32:20 -0500

    Update TicTacToeNew.java

--- TicTacToeNew.java
+++ TicTacToeNew.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 import java.util.*;
 import java.io.*;
+/*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
 public class TicTacToeNew {
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     /* part1 variables */
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static Scanner sc = new Scanner(System.in);
     static LinkedList checkDupes = new LinkedList(); //a list that holds all the spaces that have been used already
     static String player1Name, player2Name;
@@ -22,20 +25,22 @@ public class TicTacToeNew {
     static String player1Symbol;
     static String player2Symbol;
     static int retryCount;
-    static ArrayList<String> comp = new ArrayList<String>();
-    
+    static LinkedList<String> comp = new LinkedList<String>();
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     /*------------------------------------------------------*/
     /*part2 setup*/
     /*------------------------------------------------------*/
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     public static void main(String[] args){
         namesSet();
         playerSet();
-        if(player2Name.equals("comp")){
+        if(player2Name.equals("comp")){//follows a computer only game method if played by yourself
             playComp();
         }
         playerOrder();
         play();
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void namesSet(){
         //opens a scanner for the users to imput their name so its a personalixed game instead of saying player x and player o
         System.out.print(" Please, enter Player 1's name: ");
@@ -45,6 +50,7 @@ public class TicTacToeNew {
         System.out.print("  Please, enter Player 2's name: ");
         player2Name = sc.next();
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void playerSet(){
         System.out.print("\n  " + player1Name+ ", choose a symbol to play with, \"x\" or \"o\"? ");
         // tells the first player to pick what symbol they want to play as, and the other player gets the other symbol
@@ -58,7 +64,8 @@ public class TicTacToeNew {
             player2Symbol= "x";
         }
         System.out.print("  => " + player2Name+ " will play with the '" + player2Symbol + " symbol.\n");//re-states what player is which symbol
-            }
+    }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void playerOrder(){
         System.out.print("\n  " + player1Name + ", would you like to play first (y/n)? ");// asks if player 1 wants to make the forst move or not
         char playerAns = sc.next().toLowerCase().charAt(0);
@@ -72,23 +79,11 @@ public class TicTacToeNew {
             player2NameTurn = 0;
         }
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     /*--------------------------------------- */
     /*part3 game functions*/
     /*--------------------------------------- */
-    static void playComp(){
-        comp.clear();
-        clearBoard();//resets the board
-        draw();// draws the board
-        turnCount = 0;// resets the count to 0
-        comp.add(one); comp.add(two); comp.add(three); comp.add(four); comp.add(five); comp.add(six);comp.add(seven);comp.add(eight); comp.add(nine);
-        while (turnCount <= 8) {//runs the game as long as the count is up to 9
-                //of the first player wishes to go first order
-                player1();
-                checkWin();
-                Comp();
-                checkWin();
-        }
-    }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void play() {//runs the game in a loop for unlimited games unless told to end the loop and the code will stop running
         clearBoard();//resets the board
         draw();// draws the board
@@ -110,10 +105,12 @@ public class TicTacToeNew {
             }
         }
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void draw(){
         // creates the game board
         System.out.println(one + "|" + two + "|" + three + "\n" + four + "|" + five +"|" + six + "\n" + seven + "|" + eight + "|" + nine +"\n");
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void clearBoard(){
         checkDupes.clear(); // clears the linkedlist so it can be empty for a new game
         one = "1";//sets all the locations back to their original numbers
@@ -127,6 +124,7 @@ public class TicTacToeNew {
         nine = "9";
         turnCount = 0; // resets the turn count back to 0
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void player1(){//allows the 1st player to  make their moves or tells them that their moves are invalid, prints the game board with the new turn, counts the number of turns, and checks for a win
         turnCount= turnCount+1; //adds to the turn count for evey move
         System.out.println(player1Name+", enter the number that corresponds to the cell: "); // directions to place a move
@@ -134,54 +132,77 @@ public class TicTacToeNew {
         if(turnCount>=4){//checks the win if there are 5 or more moves played because the is a possibility of a win for the player who goes first
             checkWin();
         }
-        //if(player2Name.equals("comp")){
-        //    int location = Integer.parseInt(choice);
-        //    comp.remove(location-1);
-        //}
         //shows all the options for a player mocing into one of the 9 spaces avalible and turns the designated location into the players number, only for player 1
         if(one.equals(choice) && !checkDupes.contains(choice)){//checks if the location has ben used already
             one =player1Symbol;
             checkDupes.add(choice);//adds the designated location into the linkedlist so it cannot be used again
+            if(player2Name.equals("comp")){//only in the case of playing the computer, the comp linked list gets erased one by one to limit its options of play locations
+            comp.remove(choice);//
+            }
             draw();//re-draws the new game table with the charcter in the designated location
         }
         else if(two.equals(choice) && !checkDupes.contains(choice)){
             two =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else if(three.equals(choice) && !checkDupes.contains(choice)){
             three =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else if(four.equals(choice) && !checkDupes.contains(choice)){
             four =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else if(five.equals(choice) && !checkDupes.contains(choice)){
             five =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else if(six.equals(choice) && !checkDupes.contains(choice)){
             six =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else if(seven.equals(choice) && !checkDupes.contains(choice)){
             seven =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else if(eight.equals(choice) && !checkDupes.contains(choice)){
             eight =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
                 }
         else if(nine.equals(choice) && !checkDupes.contains(choice)){
             nine =player1Symbol;
             checkDupes.add(choice);
+            if(player2Name.equals("comp")){
+            comp.remove(choice);
+            }
             draw();
         }
         else{
@@ -199,6 +220,7 @@ public class TicTacToeNew {
         }
         retryCount=0;
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     public static void player2(){//allows the 2nd player to  make their moves or tells them that their moves are invalid, prints the game board ith the new turn, counts the number of turns, and checks for a win
         turnCount= turnCount+1;//adds to the turn count for evey move
         System.out.println(player2Name+", enter the number that corresponds to the cell: ");// directions to place a move
@@ -267,10 +289,30 @@ public class TicTacToeNew {
         }
         retryCount=0;
     }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
+    /*--------------------------------------- */
+    /*part4 comp v player*/
+    /*--------------------------------------- */
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
+    static void playComp(){
+        comp.clear();//clears out the comp list so when the game gets restarted no doubles will exist in the list
+        clearBoard();//resets the board
+        draw();// draws the board
+        turnCount = 0;// resets the count to 0
+        comp.add(one); comp.add(two); comp.add(three); comp.add(four); comp.add(five); comp.add(six);comp.add(seven);comp.add(eight); comp.add(nine);
+        while (turnCount <= 8) {//runs the game as long as the count is up to 9
+                //of the first player wishes to go first order
+                player1();//player one moves
+                checkWin();
+                Comp();//computer moves by itself
+                checkWin();
+        }
+    }
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     static void Comp(){
         turnCount= turnCount+1;
-        Collections.shuffle(comp);
-        choice = comp.get(0);
+        Collections.shuffle(comp);//creates a mixed order for the computer to pick
+        choice = comp.get(0);//the first element in the mixed linked list is what gets picked
         if(turnCount>=4){//checks the win if there are 5 or more moves played because the is a possibility of a win for the player who goes first
             checkWin();
         }
@@ -278,59 +320,67 @@ public class TicTacToeNew {
         if(one.equals(choice) && !checkDupes.contains(choice)){//checks if the location has ben used already
             one =player2Symbol;
             checkDupes.add(choice);//adds the designated location into the linkedlist so it cannot be used again
+            comp.remove(choice);
             draw();//re-draws the new game table with the charcter in the designated location
         }
         else if(two.equals(choice) && !checkDupes.contains(choice)){
             two =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(three.equals(choice) && !checkDupes.contains(choice)){
             three =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(four.equals(choice) && !checkDupes.contains(choice)){
             four =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(five.equals(choice) && !checkDupes.contains(choice)){
             five =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(six.equals(choice) && !checkDupes.contains(choice)){
             six =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(seven.equals(choice) && !checkDupes.contains(choice)){
             seven =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(eight.equals(choice) && !checkDupes.contains(choice)){
             eight =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else if(nine.equals(choice) && !checkDupes.contains(choice)){
             nine =player2Symbol;
             checkDupes.add(choice);
+            comp.remove(choice);
             draw();
         }
         else{
         Comp();
         }
         Collections.sort(comp);
-        //int location = Integer.parseInt(choice);
-           // comp.remove(location-1);
         }
-
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     /*--------------------------------------- */
-    /*part4 game check*/
+    /*part5 game check*/
     /*--------------------------------------- */
+    /*--------------------------------------------------------------------------------------------------------------------------------------------------------- */
     public static void checkWin(){
         //checks all of the 9 possibilities of a win for a player
         if(one.equals(two) && two.equals(three)){
